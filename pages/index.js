df68475c3c3a6ff352d4477c8b5c0dccdf1ab2f2
sergeylukin <sergeylukin@insomnia247.nl>
2019-03-31 17:03:06 +0200

    Initial commit

---
+++ pages/index.js
@@ -0,0 +1,27 @@
+if (typeof document !== 'undefined' && document) {
+  var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+  (function(){
+  var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+  s1.async=true;
+  s1.src='https://embed.tawk.to/5ca0d6821de11b6e3b061801/default';
+  s1.charset='UTF-8';
+  s1.setAttribute('crossorigin','*');
+  s0.parentNode.insertBefore(s1,s0);
+  })();
+}
+
+const Index = () => (
+  <div>
+    <img src="/static/logo.jpg" width="680" />
+      <style jsx>{`
+        img {
+          position: absolute;
+          top: 50%;
+          left: 50%;
+          margin: -230px 0 0 -340px;
+        }
+      `}</style>
+  </div>
+)
+
+export default Index
